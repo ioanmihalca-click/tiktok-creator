86f526ce409ba7b7b4c4eaf76574449e4a4f3d0c
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-03-03 22:11:53 +0200

    cashier

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Laravel\Cashier\Cashier;
 use App\Services\AI\CategoryService;
-use App\Services\AI\ImageGenerationService;
 use App\Services\AI\NarrationService;
-use App\Services\AI\ScriptGenerationService;
+use Illuminate\Support\ServiceProvider;
+use App\Services\AI\ImageGenerationService;
 use App\Services\AI\TopicGenerationService;
 use App\Services\AI\VideoGenerationService;
-use Illuminate\Support\ServiceProvider;
+use App\Services\AI\ScriptGenerationService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
             return new CategoryService();
         });
 
-        $this->app->singleton(TopicGenerationService::class);
+
         $this->app->singleton(ScriptGenerationService::class);
         $this->app->singleton(ImageGenerationService::class);
         $this->app->singleton(NarrationService::class);
@@ -33,6 +35,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Cashier::useCustomerModel(User::class);
     }
 }
